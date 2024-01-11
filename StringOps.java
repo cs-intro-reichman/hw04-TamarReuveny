09bd873edd8c9b17f0c02a5781ae8db5e5efcc88
Tamar Reuveny <Tamar.reuveny@runi.ac.il>
2024-01-11 16:51:04 +0200

    edit2

--- StringOps.java
+++ StringOps.java
@@ -24,7 +24,7 @@ public class StringOps {
     public static void main(String[] args) {
 
         System.out.println(capVowelsLowRest("One two tHRee world"));
-         System.out.println(camelCase(" Intro to coMPUter SCIEnce "));
+         System.out.println(camelCase(" tWo wordS"));
          System.out.println(allIndexOf("Hello world", 'l'));
 
         
@@ -48,7 +48,7 @@ public class StringOps {
 
                     newString = newString + " ";
 
-            } else if (strNew >= 65 && strNew <= 90) {
+            } else if ((strNew >= 65 && strNew <= 90) && strNew != 65 && strNew != 69 && strNew != 73 && strNew != 79 && strNew != 85) {
 
                 newString = newString + (char)(strNew + 32);
 
